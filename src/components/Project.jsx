6276eef481817012afc520330e667a1a2f90bf76
Ashfaq2806 <ashfaqsadiq678@gmail.com>
2025-06-05 15:22:42 +0530

    commit

--- src/components/Project.jsx
+++
@@ -1,3 +0,0 @@
-export default function Product() {
-  return <h3>This is Product component</h3>;
-}
--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import Text from "./components/Text.jsx";
-createRoot(document.getElementById("root")).render(<App />);
